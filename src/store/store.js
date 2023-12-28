7b208f45b7c7bbd08eaca9d97be2678e887f8ecf
pavan-1805 <pavansai1806@gmail.com>
2023-12-28 21:41:36 +0530

    store changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,8 @@
-import { applyMiddleware, compose, createStore } from "redux";
+import { compose, createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const initialState = {};
 
-const store = createStore(rootReducer, initialState);
+const store = createStore(rootReducer, composeEnhancers());
 
 export default store;
